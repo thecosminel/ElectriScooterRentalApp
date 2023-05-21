8324f44c175793ce843fb1c2da4a7a7b0dab4981
George <thecosminel@gmail.com>
2023-05-21 21:52:53 +0300

    Igonoire

---
+++ cmake-build-debug/CMakeFiles/ElectricScooterRentalApp.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ElectricScooterRentalApp.dir/App.cpp.obj"
+  "CMakeFiles/ElectricScooterRentalApp.dir/App.cpp.obj.d"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppController/Controller.cpp.obj"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppController/Controller.cpp.obj.d"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppController/ControllerTest.cpp.obj"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppController/ControllerTest.cpp.obj.d"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppRepository/Repository.cpp.obj"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppRepository/Repository.cpp.obj.d"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppRepository/RepositoryTest.cpp.obj"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppRepository/RepositoryTest.cpp.obj.d"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppUI/ClientUI.cpp.obj"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppUI/ClientUI.cpp.obj.d"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppUI/MainUI.cpp.obj"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppUI/MainUI.cpp.obj.d"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppUI/ManagerUI.cpp.obj"
+  "CMakeFiles/ElectricScooterRentalApp.dir/AppUI/ManagerUI.cpp.obj.d"
+  "CMakeFiles/ElectricScooterRentalApp.dir/Domain/Scooter.cpp.obj"
+  "CMakeFiles/ElectricScooterRentalApp.dir/Domain/Scooter.cpp.obj.d"
+  "CMakeFiles/ElectricScooterRentalApp.dir/Utils/AuxiliaryFunctions.cpp.obj"
+  "CMakeFiles/ElectricScooterRentalApp.dir/Utils/AuxiliaryFunctions.cpp.obj.d"
+  "ElectricScooterRentalApp.exe"
+  "ElectricScooterRentalApp.exe.manifest"
+  "ElectricScooterRentalApp.pdb"
+  "libElectricScooterRentalApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ElectricScooterRentalApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ElectricScooterRentalApp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ElectricScooterRentalApp.
+# This may be replaced when dependencies are built.
